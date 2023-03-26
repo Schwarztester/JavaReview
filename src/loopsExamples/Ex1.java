010e968546bd9d93ca9fb8c41dc4193e0bec4fad
schwarztester <himikchi@gmail.com>
2023-03-27 00:26:03 +0200

    first example has been solved

---
+++ src/loopsExamples/Ex1.java
@@ -0,0 +1,32 @@
+package loopsExamples;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        // write a program to calculate the faktorial of a given integer
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("please give a number to calculate its faktorial: ");
+        int n= scan.nextInt();
+        int sum=1;
+        int i= 1;
+        while(i<=n){
+            sum+=i;
+            i++;
+        }
+        System.out.println("faktorial("+n+")= "+sum);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
