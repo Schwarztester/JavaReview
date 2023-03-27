ef9a10c6dfd7d2b8e393237e8049967ca1c1d9fc
schwarztester <himikchi@gmail.com>
2023-03-27 03:40:54 +0200

    second example edition 4

--- src/loopsExamples/Ex2.java
+++ src/loopsExamples/Ex2.java
@@ -7,7 +7,7 @@ public class Ex2 {
 
 
 //Write a program that ask the console for two intiger (int n and int m)
-// and displays all the ODD numbers from n to m
+// and displays all the ODD numbers from n to m  and displays the sum of the odd numbers
 
         Scanner scan = new Scanner(System.in);
         System.out.print("please enter the first number : ");
@@ -16,6 +16,7 @@ public class Ex2 {
         int m= scan.nextInt();
         int small;
         int bigger;
+        int sum=0;
         if(n>m) {
             small = m;
             bigger = n;
@@ -25,8 +26,10 @@ public class Ex2 {
         for (int i = small; i <=bigger ; i++) {
             if (i%2!=0){
                 System.out.print(i+", ");
+                sum+=i;
             }
         }
+        System.out.println("sum of the odd numbers="+sum);
 
         //second way
 
@@ -41,13 +44,16 @@ public class Ex2 {
             a=temp;}
 
 
-
+           sum=0;
        while (a<=b) {
             if (a%2!=0){
                 System.out.print(a+", ");
+                sum+=a;
+
             }
             a++;
         }
+        System.out.println("sum of the odd numbers="+sum);
 
 
 
